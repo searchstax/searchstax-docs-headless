11695cb05f4861bcb3b0d2f60112a40e5989d7b2
searchstaxkevin <admin@MacBook-Pro.local>
2023-12-07 10:38:00 -0800

    Admin URL fix

--- build/searchstax_studio_react/dist/redirect.js
+++
@@ -1,7 +0,0 @@
-const drupalRoot = document.getElementById('main-content');
-if (drupalRoot) {
-	const reactRoot = document.getElementById('root');
-	if (!reactRoot && !window.location.pathname.startsWith("/admin")) {
-		window.location.href = "/?url=" + window.location.pathname;
-	}
-}
